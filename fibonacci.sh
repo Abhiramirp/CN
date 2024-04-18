eb509736c81de8fec37ab4efd2a0e4ceb946eeb4
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-04-18 14:44:23 +0530

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,25 @@
+echo "Enter the limit:"
+read n
+fibonacci()
+{
+ a=0
+ b=1
+ if [ $n -eq 1 ]
+ then
+  echo "fibonacci series:$a"
+ elif [ $n -eq 2 ]
+ then
+  echo "fibonacci series:$a $b"
+  else
+  echo "$a"
+  echo "$b"
+  for (( i=2 ; i < $n ; i++ )) 
+  do 
+  c=$((a + b))
+  a=$b
+  b=$c
+  echo "$c"
+ done
+fi
+}
+fibonacci
