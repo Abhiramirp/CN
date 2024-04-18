f9fd6c98f8582c61d29c1b39b540d644623caba7
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-04-18 14:47:05 +0530

    Create decimaltobinary.sh

---
+++ decimaltobinary.sh
@@ -0,0 +1,13 @@
+echo "Enter a decimal number:"
+read decimal
+binary=0
+i=1
+while [ $decimal -ne 0 ]
+do
+  rem=$((decimal % 2))
+  decimal=$((decimal / 2))
+  binary=$((binary+rem*i))
+  i=$((i*10))
+done
+echo "the binary equivalent is:$binary "
+ 
